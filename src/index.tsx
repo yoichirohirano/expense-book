811ee650679d2a4cef0204d9aa7c463fac5482f3
Yoichiro Hirano <y.hirano.0104@gmail.com>
2020-05-08 19:45:25 +0900

    [update] re-ducks pattern redux state

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "./state/store";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import "typeface-roboto";
