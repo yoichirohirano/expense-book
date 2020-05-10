3ce7838962484a5f7589adcc1ad15088a085a6e3
Yoichiro Hirano <y.hirano.0104@gmail.com>
2020-05-11 07:56:20 +0900

    [add] redux to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
+import React from "react";
 import { addDecorator, addParameters } from "@storybook/react";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import { muiTheme } from "storybook-addon-material-ui";
+import { Provider } from "react-redux";
 import theme from "../src/materialUI/theme.js";
+import store from "../src/state/store";
 
 addDecorator(muiTheme(theme));
+
+addDecorator(story => <Provider store={store}>{story()}</Provider>);
 
 addParameters({
   viewport: {
