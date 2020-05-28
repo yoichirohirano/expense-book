97286b9175c3dbfecf48b184152075b13397c3ce
Yoichiro Hirano <y.hirano.0104@gmail.com>
2020-05-29 07:40:22 +0900

    [change] delete CategoryEditList

--- src/components/organisms/CategorySetting/style.tsx
+++ src/components/organisms/CategorySetting/style.tsx
@@ -1,6 +1,6 @@
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     root: {
       paddingBottom: "20px",
