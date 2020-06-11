fc3048a3a2bdbc528862999a7a95534365bc9e1f
Yoichiro Hirano <y.hirano.0104@gmail.com>
2020-06-11 15:49:14 +0900

    [update] DB Scheme change - Catergory

--- src/components/atoms/ExpenseChart/index.test.tsx
+++ src/components/atoms/ExpenseChart/index.test.tsx
@@ -1,31 +1,36 @@
 import React from "react";
-import ExpenseChart from "./";
+import ExpenseChart, { ExpenseChartProps } from "./";
 import { shallow } from "enzyme";
+
+export const expenseChartSampleProps: ExpenseChartProps = {
+  chartItems: [
+    {
+      categoryName: "Food",
+      amount: 24000,
+      budget: 30000,
+      sortIndex: 0,
+      color: "#489ec8",
+    },
+    {
+      categoryName: "Cafe",
+      amount: 7000,
+      budget: 5000,
+      sortIndex: 1,
+      color: "#489ec8",
+    },
+    {
+      categoryName: "test",
+      amount: 12000,
+      budget: 12000,
+      sortIndex: 2,
+      color: "#489ec8",
+    },
+  ],
+};
 
 describe("<ExpenseChart />", () => {
   let container: any = null;
-  const props = {
-    chartItems: [
-      {
-        categoryName: "Food",
-        amount: 24000,
-        color: "#7CB342",
-        budget: 30000,
-      },
-      {
-        categoryName: "Cafe",
-        amount: 7000,
-        color: "#D81B60",
-        budget: 5000,
-      },
-      {
-        categoryName: "test",
-        amount: 12000,
-        color: "#FDD835",
-        budget: 12000,
-      },
-    ],
-  };
+  const props: ExpenseChartProps = expenseChartSampleProps;
 
   afterEach(() => {
     container.unmount();
