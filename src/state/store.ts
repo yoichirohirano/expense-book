167105748c877d68b75a316d2a2c777e0fc3aab5
Yoichiro Hirano <y.hirano.0104@gmail.com>
2020-09-07 18:46:19 +0900

    delete: redux-persist

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,8 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { connectRouter, routerMiddleware } from "connected-react-router";
-import { persistStore, persistReducer } from "redux-persist";
 import { RouterState } from "connected-react-router";
-import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import budgets, { Budgets } from "./budgets";
 import categories, { Categories } from "./categories";
@@ -27,16 +25,9 @@ const rootReducer = combineReducers({
   router: connectRouter(history),
 });
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
 const store = createStore(
-  persistReducer(persistConfig, rootReducer),
+  rootReducer,
   composeWithDevTools(applyMiddleware(routerMiddleware(history), thunk))
 );
-
-export const persistor = persistStore(store);
 
 export default store;
