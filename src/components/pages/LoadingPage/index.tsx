93c07785af4c457d75692291dcacc665cba11fd4
Yoichiro Hirano <y.hirano.0104@gmail.com>
2020-09-10 15:03:18 +0900

    fix: path

---
+++ src/components/pages/LoadingPage/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Loading from "@/components/templates/Loading";
+
+const LoadingPage: React.FC = () => {
+  return <Loading />;
+};
+
+export default LoadingPage;
---
+++ src/components/pages/TopPage/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Top from "@/components/templates/Top";
+
+const TopPage: React.FC = () => {
+  return <Top />;
+};
+
+export default TopPage;
